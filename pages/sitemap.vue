431a858053ef6dd9ca89eaeea5bc5b65dcbefb50
unknown <403003167@qq.com>
2019-09-25 15:31:21 +0800

    a

---
+++ pages/sitemap.vue
@@ -0,0 +1,95 @@
+<template>
+  <div>
+    <div style="display: table;width: 100%;">
+      <ul>
+        <li class="title">
+          <h3>www.dnitu.com网站地图</h3>
+        </li>
+        <li class="lks">
+          <span>0</span>
+          <a href="https://www.dnitu.com/" title="令人感动的故事与笑话-泥兔网" target="_blank">令人感动的故事与笑话-泥兔网</a>
+        </li>
+        <li class="lks">
+          <span>1</span>
+          <a href="https://www.dnitu.com/" title="首页" target="_blank">首页-泥兔网</a>
+        </li>
+        <li class="lks">
+          <span>2</span>
+          <a href="https://www.dnitu.com/motivational/1/" title="励志" target="_blank">励志-泥兔网</a>
+        </li>
+        <li class="lks">
+          <span>3</span>
+          <a href="https://www.dnitu.com/education/1/" title="教育" target="_blank">教育-泥兔网</a>
+        </li>
+        <li class="lks">
+          <span>4</span>
+          <a href="https://www.dnitu.com/inspirational/1/" title="灵性" target="_blank">灵性-泥兔网</a>
+        </li>
+        <li class="lks">
+          <span>5</span>
+          <a href="https://www.dnitu.com/family/1/" title="家庭" target="_blank">家庭-泥兔网</a>
+        </li>
+        <li class="lks">
+          <span>6</span>
+          <a href="https://www.dnitu.com/love/1/" title="爱" target="_blank">爱-泥兔网</a>
+        </li>
+        <li class="lks">
+          <span>7</span>
+          <a href="https://www.dnitu.com/funny/1/" title="娱乐" target="_blank">娱乐-泥兔网</a>
+        </li>
+        <li class="lks">
+          <span>9</span>
+          <a href="https://www.dnitu.com/article/14/" title="国王和渔夫的故事" target="_blank">国王和渔夫的故事-泥兔网</a>
+        </li>
+        <li class="lks">
+          <span>11</span>
+          <a href="https://www.dnitu.com/article/13/" title="愚蠢男人的故事" target="_blank">愚蠢男人的故事-泥兔网</a>
+        </li>
+        <li class="lks">
+          <span>13</span>
+          <a href="https://www.dnitu.com/article/12/" title="有趣的小故事" target="_blank">有趣的小故事-泥兔网</a>
+        </li>
+        <li class="lks">
+          <span>15</span>
+          <a href="https://www.dnitu.com/article/11/" title="女儿给爸爸的礼物" target="_blank">女儿给爸爸的礼物-泥兔网</a>
+        </li>
+        <li class="lks">
+          <span>17</span>
+          <a href="https://www.dnitu.com/article/10/" title="两兄弟的故事" target="_blank">两兄弟的故事-泥兔网</a>
+        </li>
+        <li class="lks">
+          <span>18</span>
+          <a href="https://www.dnitu.com/motivational/1/" title="励志" target="_blank">励志-泥兔网</a>
+        </li>
+        <li class="lks">
+          <span>19</span>
+          <a href="https://www.dnitu.com/education/1/" title="教育" target="_blank">教育-泥兔网</a>
+        </li>
+        <li class="lks">
+          <span>20</span>
+          <a href="https://www.dnitu.com/inspirational/1/" title="灵性" target="_blank">灵性-泥兔网</a>
+        </li>
+        <li class="lks">
+          <span>21</span>
+          <a href="https://www.dnitu.com/family/1/" title="家庭" target="_blank">家庭-泥兔网</a>
+        </li>
+        <li class="lks">
+          <span>22</span>
+          <a href="https://www.dnitu.com/love/1/" title="爱" target="_blank">爱-泥兔网</a>
+        </li>
+        <li class="lks">
+          <span>23</span>
+          <a href="https://www.dnitu.com/funny/1/" title="娱乐" target="_blank">娱乐-泥兔网</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+<style lang="scss">
+  a:link{text-decoration: none;}a:visited{text-decoration:none;}a:active{text-decoration:none;}
+body{font-family: Arial, "微软雅黑";font-size: 13px;}
+ul, li{margin:0px; padding:0px; list-style:none;}ul{width:90%;margin-left: auto;margin-right: auto;}.title{width:100%;font-size: 18px;}
+.lks a {font-size:13px;border:1px solid #e4e4e4;width:150px;height:30px;line-height:30px;float:left;padding-left:5px;white-space:nowrap;overflow:hidden;text-overflow:ellipsis;}
+.lks span {font-size:10px;width:40px;background-color:#e4e4e469;margin-bottom:3px;text-align:center;border-left:1px solid #e4e4e4;float:left;border-top:1px solid #e4e4e4;border-bottom:1px solid #e4e4e4;}
+.lks {line-height:30px;font-size:16px;height:32px;width:200px;text-align:left;float:left;margin-bottom:3px;}
+</style>
